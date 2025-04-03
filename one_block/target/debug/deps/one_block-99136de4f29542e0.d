47fdd7733eae09ee4297ae11d10827f37ab6d5b9
Alibert <aliberth33@gmail.com>
2025-04-03 17:33:30 -0400

    first commit with cargo

---
+++ one_block/target/debug/deps/one_block-99136de4f29542e0.d
@@ -0,0 +1,5 @@
+/home/randomberth/blockchain/one_block/target/debug/deps/libone_block-99136de4f29542e0.rmeta: src/main.rs
+
+/home/randomberth/blockchain/one_block/target/debug/deps/one_block-99136de4f29542e0.d: src/main.rs
+
+src/main.rs:
